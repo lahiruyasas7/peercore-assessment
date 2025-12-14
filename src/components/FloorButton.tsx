3201c487c80089a669b00a976c9bca87f30dffcf
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-12-14 21:54:38 +0530

    feat: increased the w and h to 120px

--- src/components/FloorButton.tsx
+++ src/components/FloorButton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const FloorButton = ({ floor, active }: { floor: number; active: boolean }) => {
   return (
     <div
-      className={`w-16 h-16 flex items-center justify-center rounded-2xl text-white text-2xl font-semibold
+      className={`w-30 h-30 flex items-center justify-center rounded-2xl text-white text-2xl font-semibold
       shadow-lg transition-all duration-300
       ${active ? "bg-[#00B386] ring-4 ring-[#82E6C3]" : "bg-[#4B4C65]"}`}
     >
