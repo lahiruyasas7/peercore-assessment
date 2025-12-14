536f4e584c9ba99790b309ac9a09ca5dc21435da
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-12-14 18:15:34 +0530

    feat: created arrow buttons

---
+++ src/components/ArrowButtons.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ArrowButtons = ({ onPress }: { onPress: () => void }) => {
+  return (
+    <div
+      className="flex flex-col items-center cursor-pointer select-none"
+      onClick={onPress}
+    >
+      <div className="text-[#1C9C8C] text-xl leading-[18px]">▲</div>
+      <div className="text-[#1C9C8C] text-xl leading-[18px]">▼</div>
+    </div>
+  );
+};
+
+export default ArrowButtons;
